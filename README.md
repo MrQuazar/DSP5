0da317e214d046dc89f9e1f4c948c525cf0a5968
Aartem Singh <70425120+MrQuazar@users.noreply.github.com>
2026-01-21 15:05:55 +0000

    moving gitlab to github

--- README.md
+++
@@ -1,2 +0,0 @@
-# DSP5
-3D Safe zone based survival game for PC using UE5. 
--- Source/DSP5/DSP5.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "DSP5.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DSP5, "DSP5" );
